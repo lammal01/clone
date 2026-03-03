1ec40e99d7d1acd4a689f497356fa15ab71ea045
lammal01 <catafeel23@gmail.com>
2026-03-03 18:45:41 +0900

    CLONE

---
+++ run.sh
@@ -0,0 +1,13 @@
+echo "AIRFLOW_UID=$(id -u)" > .env
+echo "AIRFLOW_GID=$(id -g)" >> .env
+# build한 docker image 버젼을 저장
+echo "VERSION=${1}" >> .env
+
+mkdir airflow || true
+mkdir shared || true
+
+
+tmux kill-session -t "auto-trade" || true
+docker compose down
+sleep 5  # 강제 대기
+tmux new -d -s "auto-trade" docker compose up --build
